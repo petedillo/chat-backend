ce9a0ba886507edddfd60299e2f06e2d422e48ab
Pedro Delgadillo <91334249+petedillo@users.noreply.github.com>
2025-02-27 10:53:28 -0600

    remove unused imports

--- src/main/java/com/chatbackend/config/SecurityConfig.java
+++ src/main/java/com/chatbackend/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.chatbackend.config;
 
-import com.chatbackend.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
