a4111a5a48f1308123f1b3f7170eb0567eb9ea5e
Pedro Delgadillo <91334249+petedillo@users.noreply.github.com>
2025-02-27 13:10:43 -0600

    add repos for role and privilege

---
+++ src/main/java/com/chatbackend/repository/PrivilegeRepository.java
@@ -0,0 +1,8 @@
+package com.chatbackend.repository;
+
+import com.chatbackend.entity.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+    Privilege findByName(String name);
+}
